bb98aa60c54449824ec24ca02f10fc5013cd9ae1
RonWataya <ronnexwataya0@gmail.com>
2024-02-11 18:49:34 +0200

    commit

--- config/db.config.js
+++ config/db.config.js
@@ -1,38 +1,9 @@
 require('dotenv').config();
-const HOST = 'srv582.hstgr.io';
-const USER = 'u790430628_moneyhive';
-const PASSWORD = 'Hive@2023';
-const DB = 'u790430628_moneyhive';
-const PORTAWS = '3307';
-
-module.exports = {
-    HOST: HOST,
-    USER: USER,
-    PASSWORD: PASSWORD,
-    DB: DB
-        //  PORTAWS: PORTAWS
-};
-
-/*
-const HOST = 'srv582.hstgr.io';
-const USER = 'u790430628_moneyhive';
-const PASSWORD = 'Hive@2023';
-const DB = 'u790430628_moneyhive';
-const PORTAWS = '3307';
-
-module.exports = {
-    HOST: HOST,
-    USER: USER,
-    PASSWORD: PASSWORD,
-    DB: DB
-        //  PORTAWS: PORTAWS
-};
-
-const HOST = 'miwallet-database.c2vlqvpoaksp.us-west-2.rds.amazonaws.com';
+const HOST = 'hive.cnm0ouk4axh4.us-east-1.rds.amazonaws.com';
 const USER = 'admin';
-const PASSWORD = 'wataya1993';
+const PASSWORD = 'hive2022';
 const DB = 'moneyhive';
-const PORTAWS = '3307';
+const PORTAWS = '3308';
 
 module.exports = {
     HOST: HOST,
@@ -40,5 +11,22 @@ module.exports = {
     PASSWORD: PASSWORD,
     DB: DB,
     PORTAWS: PORTAWS
+};
+
+/*
+
+
+const HOST = 'srv582.hstgr.io';
+const USER = 'u790430628_moneyhive';
+const PASSWORD = 'Hive@2023';
+const DB = 'u790430628_moneyhive';
+const PORTAWS = '3307';
+
+module.exports = {
+    HOST: HOST,
+    USER: USER,
+    PASSWORD: PASSWORD,
+    DB: DB
+        //  PORTAWS: PORTAWS
 };
 */
